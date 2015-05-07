3330b008620dd334369bce7491bba11f6d955a24
张磊 <kmdjs@qq.com>
2015-05-07 13:08:35 +0800

    all in one cmt

---
+++ observe.js
@@ -0,0 +1,113 @@
+﻿/* observejs 
+ * By dnt http://kmdjs.github.io/
+ * Github: https://github.com/kmdjs/observejs
+ * MIT Licensed.
+ */
+;(function (win) {
+
+    var observe = function (target, arr,callback) {
+
+
+        var _observe = function (target, arr, callback) {
+
+            for (var prop in target) {
+              
+                if (target.hasOwnProperty(prop)) {
+                  
+                    if (callback) {
+                        if (observe.isArray(arr) && observe.isInArray(arr, prop)) {
+                            this.watch(target, prop);
+                        } else if (observe.isString(arr) && prop == arr) {
+                            this.watch(target, prop);
+                        }                       
+                    } else{
+                        this.watch(target, prop);
+                    }
+                }
+            }
+            this.target = target;
+            this.propertyChangedHandler = callback ? callback : arr;
+        }
+        _observe.prototype = {
+            "onPropertyChanged": function (prop, value,oldValue) {
+                value!== oldValue && this.propertyChangedHandler && this.propertyChangedHandler.call(this.target, prop, value, oldValue);
+            },
+            "mock": function (target) {
+                var self = this;
+                observe.methods.forEach(function (item) {
+                    target[item] = function () {
+                        var result = Array.prototype[item].apply(this, Array.prototype.slice.call(arguments));
+                        for (var cprop in this) {
+                            if (this.hasOwnProperty(cprop) && cprop != "_super" && !observe.isFunction(this[cprop])) {
+                                self.watch(this, cprop);
+                            }
+                        }
+                        if (new RegExp("\\b" + item + "\\b").test(observe.triggerStr)) {
+                            self.onPropertyChanged("array", item, arguments[0]);
+                        }
+                        return result;
+                    };
+                });
+            },
+            "watch": function (target, prop) {
+                if (prop.substr(0, 2) == "__") return;
+                var self = this;
+                if (observe.isFunction(target[prop])) return;
+
+                var currentValue = target["__" + prop] = target[prop];
+                Object.defineProperty(target, prop, {
+                    get: function () {
+                        return this["__" + prop];
+                    },
+                    set: function (value) {
+                        self.onPropertyChanged(prop, value, this["__" + prop]);
+                        this["__" + prop] = value;
+                       
+                    }
+                });
+
+                if (observe.isArray(target)) {
+                    this.mock(target);
+                }
+                if (typeof currentValue == "object") {
+                    if (observe.isArray(currentValue)) {
+                        this.mock(currentValue);
+                    }
+                    for (var cprop in currentValue) {
+                        if (currentValue.hasOwnProperty(cprop) && cprop != "_super") {
+                            this.watch(currentValue, cprop);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new _observe(target, arr, callback)
+       
+    }
+
+    observe.methods = ["concat", "every", "filter", "forEach", "indexOf", "join", "lastIndexOf", "map", "pop", "push", "reduce", "reduceRight", "reverse", "shift", "slice", "some", "sort", "splice", "unshift", "valueOf"]
+
+    observe.triggerStr = ["concat", "pop", "push", "reverse", "shift", "sort", "splice", "unshift"].join(",")
+ 
+    observe.isArray = function (obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    }
+    observe.isString = function (obj) {
+        return typeof obj === "string";
+    }
+    observe.isInArray = function (arr, item) {
+        for (var i = arr.length; --i > -1;) {
+            if (item === arr[i]) return true;
+        }
+        return false;
+    }
+    observe.isFunction = function (obj) {
+        return Object.prototype.toString.call(obj) == '[object Function]';
+    }
+     
+    if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = observe }
+    else if (typeof define === 'function' && define.amd) { define(observe) }
+    else { win.observe = observe };
+
+})(Function('return this')());
---
+++ readme.md
@@ -0,0 +1,53 @@
+##observe.js
+
+用于观察任意对象的任意变化的类库，以轻巧、实用、强大而闻名。
+
+##3分钟精通observe.js
+
+### 对象字面量
+```javascript
+var obj = { a: 1 };
+//watch obj
+observe(obj, function (name, value) {
+    console.log(name + "__" + value);//a__2 
+});
+obj.a = 2;
+```
+
+### 数组
+```javascript
+var arr = [1, 2, 3];
+//watch obj
+observe(arr, function (name, value, old) {
+    console.log(name + "__" + value+"__"+old);
+});
+arr.push(4);//array__push 
+arr[3] = 5;//3__5
+```
+
+### 复杂对象
+```javascript
+var complexObj = { a: 1, b: 2, c: [{ d: [4] }] };
+//watch complexObj
+observe(complexObj, function (name, value) {
+    console.log(name + "__" + value);    //d__100 
+});
+complexObj.c[0].d = 100;
+```
+### 普通对象
+```javascript
+var User = function (name, age) {
+    this.name = name;
+    this.age = age;
+    //watch User's instance
+    observe(this, function (name, value, oldValue) {
+        console.log(name + "__" + value + "__" + oldValue);//name__wangwu__lisi 
+    });
+}
+var user = new User("lisi", 25);
+user.name = "wangwu";
+```
+
+
+This content is released under the (http://opensource.org/licenses/MIT) MIT License.
+
