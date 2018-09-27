933891a07a8f3deebaa40c0aef2c8b8de2126287
dntzhang <dntzhang@qq.com>
2018-09-27 13:39:50 +0800

    完善 create-plugin.js

--- packages/westore-plugin/plugin/components/list/list.js
+++ packages/westore-plugin/plugin/components/list/list.js
@@ -1,8 +1,12 @@
 import create from '../../westore/create-plugin'
 
 create({
-  data:{list:[]},
+  data: { list: [] },
   attached: function () {
+    //监听所有变化
+    this.store.onChange = function (info) {
+      console.log(info)
+    }
     // 可以在这里发起网络请求获取插件的数据
     this.store.data.list = [{
       name: '电视',
@@ -16,5 +20,26 @@ create({
     }]
 
     this.update()
+
+    setTimeout(() => {
+      this.store.data.list.splice(1, 1)
+      this.update()
+    }, 1000)
+
+
+    setTimeout(() => {
+      this.store.data.list[0].name = 'westore'
+      this.update()
+    }, 2000)
+
+    setTimeout(() => {
+      this.store.data.list.push({ name: 'dntzhang', price: 1000 })
+      this.update()
+    }, 3000)
+
+    setTimeout(() => {
+      this.store.data.list[2].price = 100000
+      this.update()
+    }, 4000)
   }
 })
--- packages/westore-plugin/plugin/westore/create-plugin.js
+++ packages/westore-plugin/plugin/westore/create-plugin.js
@@ -1,39 +1,26 @@
 import diff from './diff'
 
-let originData = null
 
 export default function create(option) {
 
     const attached = option.attached
     option.attached = function () {
-
-        this.store = { data: {} }
-        Object.assign(this.store.data, option.data)
-        if (!originData) {
-            originData = JSON.parse(JSON.stringify(option.data))
-        }
+        this.store = Object.assign({ data: {} }, option.data)
+        this.__originData = JSON.parse(JSON.stringify(option.data))
         this.setData.call(this, this.store.data)
         rewriteUpdate(this)
-        if (!this.store.instances) this.store.instances = { '&plugin': [] }
-        this.store.instances['&plugin'].push(this)
         attached && attached.call(this)
     }
     Component(option)
-
 }
 
 function rewriteUpdate(ctx) {
     ctx.update = () => {
-        const diffResult = diff(ctx.store.data, originData)
-        //优化数据和组件关联定向更新
-        for (let key in ctx.store.instances) {
-            ctx.store.instances[key].forEach(ins => {
-                ins.setData.call(ins, diffResult)
-            })
-        }
+        const diffResult = diff(ctx.store.data, ctx.__originData)
+        ctx.setData(diffResult)
         ctx.store.onChange && ctx.store.onChange(diffResult)
         for (let key in diffResult) {
-            updateOriginData(originData, key, diffResult[key])
+            updateOriginData(ctx.__originData, key, diffResult[key])
         }
     }
 }
