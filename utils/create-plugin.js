4385d980ab469173e146979aaee9b479b9d55851
dntzhang <dntzhang@qq.com>
2018-09-28 03:54:55 +0800

    add readme

---
+++ utils/create-plugin.js
@@ -0,0 +1,53 @@
+import diff from './diff'
+
+let originData = null
+let globalStore = null
+
+export default function create(store, option) {
+    let opt = store
+    if (option) {
+        opt = option
+        originData = JSON.parse(JSON.stringify(store.data))
+        globalStore = store
+        globalStore.instances = []
+    }
+
+    const attached = opt.attached
+    opt.attached = function () {
+        this.store = globalStore
+        this.store.data = Object.assign(globalStore.data, opt.data)
+        this.setData.call(this, this.store.data)
+        globalStore.instances.push(this)
+        rewriteUpdate(this)
+        attached && attached.call(this)
+    }
+    Component(opt)
+}
+
+function rewriteUpdate(ctx) {
+    ctx.update = () => {
+        const diffResult = diff(ctx.store.data, originData)
+        globalStore.instances.forEach(ins => {
+            ins.setData.call(ins, diffResult)
+        })
+        ctx.store.onChange && ctx.store.onChange(diffResult)
+        for (let key in diffResult) {
+            updateOriginData(originData, key, diffResult[key])
+        }
+    }
+}
+
+function updateOriginData(origin, path, value) {
+    const arr = path.replace(/\[|(].)|\]/g, '.').split('.')
+    if (arr[arr.length - 1] == '') arr.pop()
+    let current = origin
+    for (let i = 0, len = arr.length; i < len; i++) {
+        if (i === len - 1) {
+            current[arr[i]] = value
+        } else {
+            current = current[arr[i]]
+        }
+    }
+}
+
+create.store = globalStore
---
+++ utils/readme.md
@@ -0,0 +1,5 @@
+# 文件说明
+
+* create.js  是 westore 1.0
+* create2.js 是 westore 2.0
+* create-plugin.js 是 westore 1.0 插件开发库
