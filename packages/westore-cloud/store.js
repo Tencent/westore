ba382be2a52f788e5cbe189a8cf8b58264360549
dntzhang <dntzhang@qq.com>
2018-10-01 08:14:11 +0800

    todo

--- packages/westore-cloud/store.js
+++ packages/westore-cloud/store.js
@@ -2,7 +2,10 @@ export default {
   data: {
     item: {
         //collectionName:item
-        'user':{}
+        'user':{
+          //id可动态设置，调用 pull 或者 puhs 的时候必须设置好 id
+          //id:'W7FWJQ6qgQy38iWu'
+        }
     },
     list: {
         //collectionName:list
