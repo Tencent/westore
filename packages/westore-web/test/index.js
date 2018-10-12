ea22b0eca218663bfddb8cb435c14cecfd0bc1b6
dntzhang <dntzhang@qq.com>
2018-10-12 09:55:37 +0800

    update readme

--- packages/westore-web/test/index.js
+++ packages/westore-web/test/index.js
@@ -1,7 +1,5 @@
 var compile = require('../scripts/index')
 
-//TODO block template 
-
 var wxml1 = `
 <view wx:for="{{[1, 2, 3, 4, 5, 6, 7, 8, 9]}}" wx:for-item="i">
   <view wx:for="{{[1, 2, 3, 4, 5, 6, 7, 8, 9]}}" wx:for-item="j">
