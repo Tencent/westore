df221d554fedc8e4068462e92ea10ebdea8c4991
dntzhang <dntzhang@qq.com>
2018-10-24 09:14:02 +0800

    fix bug

--- packages/westore-cloud/store.js
+++ packages/westore-cloud/store.js
@@ -1,19 +1,20 @@
 export default {
   data: {
     //user 对应 db 的 collectionName
-    'user':[],
+    'user': [],
     //其他 collection 可以继续添加
     'product': []
   },
-  methods:{
+  methods: {
     //这里可以扩展 collection 每项的方法
-    'product':{
-      'agentString':function(){
+    'product': {
+      'agentString': function () {
         return this.agent.join('-')
       }
     }
   },
-  env:'test-06eb2e'
+  env: 'test-06eb2e',
+  updateAll: true
 
 }
 
