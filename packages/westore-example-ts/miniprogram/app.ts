8990974abadcabf2d32288fb4a6bcd53f7dd888a
YunYouJun <me@yunyoujun.cn>
2021-09-24 17:38:35 +0800

    chore: init miniprogram ts template

---
+++ packages/westore-example-ts/miniprogram/app.ts
@@ -0,0 +1,18 @@
+// app.ts
+App<IAppOption>({
+  globalData: {},
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res.code)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      },
+    })
+  },
+})
---
+++ packages/westore-example-ts/miniprogram/pages/logs/logs.ts
@@ -0,0 +1,19 @@
+// logs.ts
+// const util = require('../../utils/util.js')
+import { formatTime } from '../../utils/util'
+
+Page({
+  data: {
+    logs: [],
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map((log: string) => {
+        return {
+          date: formatTime(new Date(log)),
+          timeStamp: log
+        }
+      }),
+    })
+  },
+})
