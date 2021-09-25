00768fede998714f79d2bee82ba5a2c57f0f45da
yuqing521 <1581446178@qq.com>
2021-09-25 22:03:07 +0800

    feat: add build minified esm & cjs

---
+++ packages/westore/rollup.config.dev.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
+
+const pkg = require('./package.json')
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.module,
+      format: 'module',
+      sourcemap: true
+    },
+    {
+      file: pkg.commonjs,
+      format: 'cjs',
+      exports: 'auto',
+      sourcemap: true
+    }
+  ],
+  plugins: [resolve(), commonjs(), typescript(), sourceMaps()]
+}
--- packages/westore/rollup.config.prod.js
+++ packages/westore/rollup.config.prod.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import sourceMaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
@@ -9,15 +10,16 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.module,
+      file: pkg['module:minified'],
       format: 'module',
       sourcemap: true
     },
     {
-      file: pkg.commonjs,
+      file: pkg['commonjs:minified'],
       format: 'cjs',
+      exports: 'auto',
       sourcemap: true
     }
   ],
-  plugins: [resolve(), commonjs(), typescript(), sourceMaps()]
+  plugins: [resolve(), commonjs(), typescript(), sourceMaps(), terser()]
 }
