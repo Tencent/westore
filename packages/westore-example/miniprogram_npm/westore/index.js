d5b59a43f33576d056297c00560aa0430c6ff1fc
dntzhang <dntzhang@qq.com>
2021-09-25 20:21:06 +0800

    chore: rebuild npm

--- packages/westore-example/miniprogram_npm/westore/index.js
+++ packages/westore-example/miniprogram_npm/westore/index.js
@@ -4,7 +4,7 @@ var __DEFINE__ = function(modId, func, req) { var m = { exports: {}, _tempexport
 var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = __MODS__[modId].m; m._exports = m._tempexports; var desp = Object.getOwnPropertyDescriptor(m, "exports"); if (desp && desp.configurable) Object.defineProperty(m, "exports", { set: function (val) { if(typeof val === "object" && val !== m._exports) { m._exports.__proto__ = val.__proto__; Object.keys(val).forEach(function (k) { m._exports[k] = val[k]; }); } m._tempexports = val }, get: function () { return m._tempexports; } }); __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); } return __MODS__[modId].m.exports; };
 var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
 var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
-__DEFINE__(1632568133633, function(require, module, exports) {
+__DEFINE__(1632569496947, function(require, module, exports) {
 const ARRAYTYPE = '[object Array]'
 const OBJECTTYPE = '[object Object]'
 const FUNCTIONTYPE = '[object Function]'
@@ -129,6 +129,7 @@ if (!exports.__esModule) Object.defineProperty(exports, "__esModule", { value: t
 
   bind(keyOrView, view) {
     if (arguments.length === 1) {
+      this.data = keyOrView.data
       this.views[this._westoreViewId++] = keyOrView
     } else {
       //设置回 view 的 data，不然引用地址 错误
@@ -148,7 +149,7 @@ if (!exports.__esModule) Object.defineProperty(exports, "__esModule", { value: t
   }
 };exports.Store = Store
 }, function(modId) {var map = {}; return __REQUIRE__(map[modId], modId); })
-return __REQUIRE__(1632568133633);
+return __REQUIRE__(1632569496947);
 })()
 //miniprogram-npm-outsideDeps=["rfdc"]
 //# sourceMappingURL=index.js.map
