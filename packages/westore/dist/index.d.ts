21bf557f058b856c32e64d2f2c335c1cb25ece7c
yuqing521 <1581446178@qq.com>
2021-09-25 22:13:59 +0800

    feat: add type declaration file d.ts

---
+++ packages/westore/dist/index.d.ts
@@ -0,0 +1,17 @@
+declare function diffData(current: any, previous: any): any;
+declare function update(view: any, callback?: any): void;
+declare class Store {
+    views: {};
+    data: any;
+    constructor();
+    bind(key: any, view: any): void;
+    update(viewKey: any): void;
+}
+
+declare const _default: {
+    diffData: typeof diffData;
+    update: typeof update;
+    Store: typeof Store;
+};
+
+export { _default as default };
--- packages/westore/rollup.config.dev.js
+++ packages/westore/rollup.config.dev.js
@@ -2,24 +2,34 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import { terser } from 'rollup-plugin-terser'
+import dts from 'rollup-plugin-dts'
 
 const pkg = require('./package.json')
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.module,
-      format: 'module',
-      sourcemap: true
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.module,
+        format: 'module',
+        sourcemap: true
+      },
+      {
+        file: pkg.commonjs,
+        format: 'cjs',
+        exports: 'auto',
+        sourcemap: true
+      }
+    ],
+    plugins: [resolve(), commonjs(), typescript(), sourceMaps()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: pkg.types,
+      format: 'module'
     },
-    {
-      file: pkg.commonjs,
-      format: 'cjs',
-      exports: 'auto',
-      sourcemap: true
-    }
-  ],
-  plugins: [resolve(), commonjs(), typescript(), sourceMaps()]
-}
+    plugins: [dts()]
+  }
+]
